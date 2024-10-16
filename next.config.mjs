d3c1df4f3cf7170c025d4cc8c122673bbeae5456
Muhideen Mujeeb Adeoye <mujeeb.muhideen@gmail.com>
2024-10-16 17:22:50 +0100

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
 
 if (process.env.NEXT_PUBLIC_TEMPO) {
   nextConfig["experimental"] = {
-    swcPlugins: [[require.resolve("tempo-devtools/swc"), {}]],
+    swcPlugins: [["tempo-devtools/swc", {}]],
   };
 }
 
